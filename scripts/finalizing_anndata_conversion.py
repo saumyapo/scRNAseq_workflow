81ffc78f56720be2219baf2221657b8af3c66d6a
Saumya Pothukuchi <144373823+saumyapo@users.noreply.github.com>
2025-06-03 13:50:22 -0400

    Update and rename finalising_anndata_conversion.py to finalizing_anndata_conversion.py

--- scripts/finalizing_anndata_conversion.py
+++ scripts/finalizing_anndata_conversion.py
@@ -3,30 +3,34 @@ import numpy as np
 
 adata = sc.read_h5ad("seurat_object_new.h5ad")
 
-# Check metadata 
+# 1. Check metadata 
 adata.obs
 
 # SeuratDisk and sceasy (amongst other packages), commonly convert cluster/label information into numeric values so those need to be fixed
 
-# Converting seurat clustering information into string from numeric continuous values
+# 2. Converting seurat clustering information into string from numeric continuous values
 adata.obs['seurat_clusters'] = adata.obs['seurat_clusters'].astype(str)
 
-# Cluster information gets lost in conversion and only the index numeric values are retained. So annotation information needs to be added back to new anndata
+# 3. Cluster information gets lost in conversion and only the index numeric values are retained. So annotation information needs to be added back to new anndata
 cluster_mapping = { 
                         "0" : "Plasma",
                         "1" : "Plasma",
                         "2" : "T cells"
                   }
 
-# Apply mapping to seurat_clusters indexing
+# 4. Apply mapping to seurat_clusters indexing
 adata.obs['cluster_label'] = adata.obs['seurat_clusters'].map(cluster_mapping)
 
-# Change data type of column to categorical
+# 5. Change data type of column to categorical
 adata.obs["cluster_label"] = adata.obs["cluster_label"].astype("category")
 
-# Check UMAP to see changes are in place and are correct
+# 6. Check UMAP to see changes are in place and are correct
 sc.pl.umap(adata, color = "seurat_clusters", title='')
 sc.pl.umap(adata, color = "cluster_label", title='')
 
-# Check normalised counts. The output should reflect a Compressed Sparse Row format, ideally in numpy.float32 for efficiency 
+# 7. Check counts. The output should reflect a Compressed Sparse Row format, ideally in numpy.float32 for efficiency 
 adata.raw.X
+adata.X
+
+# 8. If required scaling can be done again (since only normalized and raw counts were converted)
+sc.pp.scale(adata, max_value=10)
